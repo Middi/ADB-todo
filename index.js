2c9c509204b3ee851e3f7041324dbc17ce049562
“Richard Middleton” <Richard@richardmiddleton.me>
2018-05-01 21:04:59 +0200

    post routes and body parser

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require('express'),
         app = express(),
-       port = process.env.PORT || 3000;
+       port = process.env.PORT || 3000,
+       bodyParser = require('body-parser');
 
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 // Routes
 var homeRoutes = require('./routes/home');
 var todoRoutes = require('./routes/todos');
