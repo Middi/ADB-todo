9fd10458eed6a94bf941a3811b88eda433f8cafa
“Richard Middleton” <Richard@richardmiddleton.me>
2018-05-02 15:38:36 +0200

    serve static assets

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-var express = require('express'),
-        app = express(),
-       port = process.env.PORT || 3000,
-       bodyParser = require('body-parser');
+const express = require('express'),
+          app = express(),
+         port = process.env.PORT || 3000,
+   bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/views'));
+
+
 // Routes
 var homeRoutes = require('./routes/home');
 var todoRoutes = require('./routes/todos');
--- routes/todos.js
+++ routes/todos.js
@@ -1,7 +1,7 @@
-var express = require('express'),
-     router = express.Router(),
-         db = require('../models'),
-         helpers = require('../helpers/todos');
+const express = require('express'),
+       router = express.Router(),
+           db = require('../models'),
+      helpers = require('../helpers/todos');
 
 
 router.route('/')
