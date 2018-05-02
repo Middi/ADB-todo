2006f4068b0d1e86cf002f8deb0d42d7b371d1a2
“Richard Middleton” <Richard@richardmiddleton.me>
2018-05-02 17:29:48 +0200

    create on the dom

--- public/app.js
+++ public/app.js
@@ -1,5 +1,9 @@
 const url = '/api/todos';
+const list = document.querySelector('.list');
+const input = document.getElementById('todoInput');
 
+
+// Get Todos
 fetch(url)
     .then((res) => {
         if (!res.ok) {
@@ -11,10 +15,50 @@ fetch(url)
     .then((res) => addTodos(res))
     .catch((err) => console.log(err));
 
+
 function addTodos(todos) {
-    const list = document.querySelector('.list');
     todos.forEach((todo) => {
-        const newTodo = `<li class="task">${todo.name}</li>`;
-        list.innerHTML += newTodo;
+        addTodo(todo);
     });
 }
+
+function addTodo(todo) {
+    let newTodo
+    if (!todo.completed) {
+        newTodo = `
+            <li class="task">${todo.name} <span>X</span></li>
+        `;
+    }
+    else {
+        newTodo = `
+            <li class="task done">${todo.name} <span>X</span></li>
+        `;
+    }
+    list.innerHTML += newTodo;
+}
+
+function createTodo() {
+    const inputVal = input.value;
+
+    const data = JSON.stringify({name: inputVal});
+
+    fetch(url, {
+        method: "POST",
+        body: data,
+        headers: {
+            'Content-Type': 'application/json'
+        }
+      })
+      .then(input.value = '')
+      .then(res => res.json())
+      .catch(err => console.error('Error:', err))
+      .then(res => addTodo(res));
+}
+
+// Event Listener
+input.addEventListener("keyup", (e) => {
+    if (e.key === "Enter") {
+        e.preventDefault();
+        createTodo();
+    }
+});
